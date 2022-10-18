82be2fcd64ae566d03cf7861f996be8f3822e07e
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2022-10-18 23:43:34 +0530

    Create EvenOdd.c

---
+++ If Else/EvenOdd.c
@@ -0,0 +1,14 @@
+//WAP to check whether a number is even or odd
+#include <stdio.h>
+int main()
+{
+	int num;
+	printf("Enter any number: ");
+	scanf("%d",&num);
+	if(num%2==0)
+       {printf("The Number %d is even",num);}    
+
+	else
+	   {printf("The number is odd");}
+return 0;
+}
