a9248903780dd2888d7d92d86b02b5d5caf6f765
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2022-10-26 22:06:01 +0530

    Create Swap.c

---
+++ Maths/Swap.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c,s;
+  
+  printf("Enter two values: "); 
+  scanf("%d%d", &a,&b);
+  printf("Swap values \n 1.using third variable \n 2.whithout using third variable \n ");
+  scanf("%d",&s);
+  
+  if(s==1)
+  {
+   c = a;
+   a = b;
+   b = c;
+   printf("a=%d b=%d" ,a,b);
+  }
+   
+  else if(s==2)
+  {
+  a = a+b;
+  b = a-b;
+  a = a-b;
+  printf("a=%d b=%d",a,b);
+  }
+  
+  else printf("Error");
+  return 0;
+  }
+  
+  
