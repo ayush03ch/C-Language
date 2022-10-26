14d73efc2952f8dd81b9eddadd11572d44f77ee6
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2022-10-26 22:06:32 +0530

    Create Temp.c

---
+++ Maths/Temp.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int t,k;
+  float f,s;
+  
+  printf("Enter Temperature in Celsius: ");
+  scanf("%d",&t);
+  
+  s = (1.8)*t;
+  f = s+32;
+  k = t+273;
+  
+  printf("Temperature in Fahrenheit = %f \n Kelvin = %d \n",f,k);
+  
+  return 0;
+  } 
