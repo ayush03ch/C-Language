7a843dcefe5dbdb07d50e4702fd01c0747c619f6
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2022-10-30 15:05:09 +0530

    Inverted Triangle Pattern of  " * "

---
+++ Patterns/P2_II.c
@@ -0,0 +1,15 @@
+//WAP to create inverted triangle pattern of *
+#include<stdio.h>
+int main()
+{ int i,j,n;
+printf("Enter any side length: ", n);
+scanf("%d",&n);
+
+  for(i=n;i>=0;i--){
+   for(j=0; j<=i; j++){
+    printf("* ");
+  }
+	printf("\n");  
+  }
+	return 0;
+}
