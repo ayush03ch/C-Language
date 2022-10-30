bb56b0237e233e54e17244bff2b0879fb1c66bb8
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2022-10-30 15:05:42 +0530

    Triangle pattern

---
+++ Patterns/P2_III.c
@@ -0,0 +1,20 @@
+//WAP to print triangle pattern aligned right using *
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter number of rows: ");
+	scanf("%d",&n);
+  
+    for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{ if((i+j)<=5)
+		  printf("  ");
+		 else printf("* "); 
+		} 
+		 printf("\n");
+	} 
+
+return 0;
+}
