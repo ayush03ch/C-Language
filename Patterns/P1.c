3aac81f1c5e0bb600fe41eb0e832577f91adb113
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2022-10-30 15:03:32 +0530

    Square Pattern of  " * "

---
+++ Patterns/P1.c
@@ -0,0 +1,13 @@
+//WAP to create square shaped pattern form *
+#include<stdio.h>
+int main()
+{ 
+	int i,j;
+	for(i=0;i<=5;i++)
+	{ for(j=0; j<=5;j++)
+	  printf("* ");
+	  printf("\n");
+	}
+    
+	return 0;
+}
