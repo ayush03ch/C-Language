dc37ffc5e346ed940b6242c6613ff61d0f768327
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2023-08-25 21:04:48 +0530

    Add files via upload

---
+++ Maths/SquareRoot.c
@@ -0,0 +1,12 @@
+//WAP to calculate square root of a number
+#include <math.h>
+#include <stdio.h>
+int main()
+{ int a,c;
+printf("Enter any number: ");
+scanf("%d", &a);
+c=sqrt(a);
+  printf("Square root of given number is %d", c);
+
+return 0;
+}
