4d14e08d00e09d1589d92ccb31aca41f4625fffd
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2024-01-20 22:09:19 +0530

    Add files via upload

---
+++ Patterns/Pyramid_I.c
@@ -0,0 +1,20 @@
+//WAP to make a pyramid pattern using *
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter number of rows you want to print: ");
+	scanf("%d",&n);
+
+	for(int i=1;i<=n;i++)
+	{
+		for(int space=1; space<=(n-i); space++)
+		printf("  ");
+
+		for(int j=1; j<=i; j++)
+		printf(" *  ");
+
+        printf("\n");
+	}
+return 0;
+}
