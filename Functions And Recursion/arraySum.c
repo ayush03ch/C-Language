b5aec4b29ff72dbca4392570919ace2c9012c385
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2024-03-04 13:56:12 +0530

    Add files via upload

---
+++ Functions And Recursion/arraySum.c
@@ -0,0 +1,21 @@
+//WAP to calculate summ of array elements using recursion
+#include<stdio.h>
+int arraysum(int arr[],int n)
+{
+	if(n<=0) return 0;
+	else return arraysum(arr,n-1) + arr[n-1];
+}
+
+int main()
+{
+	int n;
+	printf("Enter size of array: ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter elements of array: ");
+	for(int i=0;i<n;i++)
+	scanf("%d",&arr[i]);
+	int m = arraysum(arr,n);
+	printf("Sum of all elements in the array is %d",m);
+return 0;
+}
